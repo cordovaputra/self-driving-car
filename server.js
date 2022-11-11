13826cab667f0b8f57ae622636f1e4550630a552
Cordova <cordovansyah@gmail.com>
2022-11-11 13:10:21 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+const server = http.createServer(app)
+
+// Listen to Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/javascript', express.static(__dirname + 'public/javascript'));
+app.use('/image', express.static(__dirname + 'public/image'));
+
+
+// Listen to HTML
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(3000,() => console.log('Server running on port 3000'));
+
